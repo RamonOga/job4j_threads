e90398eefbbac45a9fee212df172476188afc1c0
Roman Markelov <Roman.sercent@gmail.com>
2021-05-14 14:22:21 +0300

    1. Запуск нити.

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -1,4 +1,19 @@
 package ru.job4j.concurrent;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ConcurrentOutput {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        Thread thread2 = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+            System.out.println(Thread.currentThread().getName());
+            thread1.start();
+            thread2.start();
+    }
 }
