80b38982d3dedc7f3b749dca127abfc12db48093
Ramon Markelov <Roman.sercent@gmail.com>
2021-05-22 17:02:50 +0300

    synchronized method

--- src/main/java/ru/job4j/concurrent/Cache.java
+++ src/main/java/ru/job4j/concurrent/Cache.java
@@ -3,15 +3,10 @@ package ru.job4j.concurrent;
 public final class Cache {
     private static Cache cache;
 
-    public static Cache instOf() {
-        Object monitor = new Object();
+    public static synchronized Cache instOf() {
        if (cache == null) {
-           synchronized (monitor) {
-               if (cache == null) {
                    cache = new Cache();
                }
-           }
-        }
         return cache;
     }
 }
