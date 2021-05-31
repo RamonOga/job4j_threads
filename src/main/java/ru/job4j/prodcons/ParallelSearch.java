719248e26437dc4791ec8023cc667d3f3f4c1f13
Roman Markelov <Roman.sercent@gmail.com>
2021-05-31 10:32:03 +0300

    2. Обеспечить остановку потребителя

--- src/main/java/ru/job4j/prodcons/ParallelSearch.java
+++ src/main/java/ru/job4j/prodcons/ParallelSearch.java
@@ -3,26 +3,45 @@ package ru.job4j.prodcons;
 public class ParallelSearch {
 
     public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+
+
         final Thread consumer = new Thread(
                 () -> {
-                    for (int index = 0; index != 3; index++) {
-                            System.out.println(queue.poll());
+                    boolean run = true;
+                    while (run) {
+                        try {
+                            queue.poll();
+
+                        } catch (InterruptedException e) {
+                            run = false;
+                        }
                     }
                 }
         );
         consumer.start();
-        new Thread(
+        final Thread produce = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
-                        queue.offer(index);
+
                         try {
+                            queue.offer(index);
                             Thread.sleep(500);
+
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 }
-        ).start();
+        );
+        produce.start();
+        try {
+            produce.join();
+            consumer.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
