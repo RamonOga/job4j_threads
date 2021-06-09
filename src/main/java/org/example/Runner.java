d559d8913990e7b9231235c562a479eef1a3454b
Ramon Markelov <Roman.sercent@gmail.com>
2021-06-09 21:22:01 +0300

    answers for exam

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import ru.job4j.pool.ThreadPool;
+
+import java.util.concurrent.*;
+
 public class Runner {
     public static void main(String[] args) {
-
+        ExecutorService es = Executors.newFixedThreadPool(10);
+        Future<Integer> f = es.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return 15;
+            }
+        });
+        try {
+            System.out.println(f.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
     }
 }
